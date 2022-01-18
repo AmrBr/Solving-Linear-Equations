e2566c2f605697dcfeb7771eb2d209fd76d024ce
Adham <adhamwaelsalem>
2022-01-18 15:49:42 +0200

    Functions

---
+++ functions.py
@@ -0,0 +1,93 @@
+import numpy
+
+
+def LU_Decomposition(A, B):
+    size = len(A)
+    L = [[0 for i in range(size)] for j in range(size)]
+
+    for i in range(size):
+
+        # Process L and U
+        L[i][i] = 1
+        for j in range(i+1, size):
+            factor = A[j][i] / A[i][i]
+            L[j][i] = factor
+            A[j] = [a - b for a, b in zip(A[j], [k * factor for k in A[i]])]
+
+    # Forward Substitution
+    D = [0.0 for i in range(size)]
+    for i in range(size):
+        temp = 0
+        for j in range(i):
+            temp += D[j]*L[i][j]
+        D[i] = B[i] - temp
+
+    # Backward Substitution
+    X = [0 for i in range(size)]
+    for i in range(size - 1, -1, -1):
+        temp = 0
+        for j in range(i + 1, size):
+            temp += X[j] * A[i][j]
+        X[i] = (D[i] - temp) / A[i][i]
+
+    return X
+
+
+def Gauss_Elimination(A):
+
+    size = len(A)
+    for i in range(size):
+
+        # Partial Pivoting
+        pivot = i
+        for j in range(i + 1, size):
+            if A[j][i] > A[pivot][i]:
+                pivot = j
+        if pivot != i:
+            A[pivot], A[i] = A[i], A[pivot]
+
+        # Eliminating
+        for j in range(i + 1, size):
+            factor = A[j][i] / A[i][i]
+            A[j] = [a - b for a, b in zip(A[j], [k * factor for k in A[i]])]
+
+        # # Scaling
+        # factor = 1 / A[i][i]
+        # A[i] = [k * factor for k in A[i]]
+
+    # Backward Substitution
+    X = [0 for i in range(size)]
+    for i in range(size - 1, -1, -1):
+        temp = 0
+        for j in range(i + 1, size):
+            temp += X[j] * A[i][j]
+        X[i] = (A[i][-1] - temp) / A[i][i]
+
+    return X
+
+
+def Gauss_Jordan(A):
+    size = len(A)
+    for i in range(size):
+
+        # Partial Pivoting
+        pivot = i
+        for j in range(i + 1, size):
+            if A[j][i] > A[pivot][i]:
+                pivot = j
+        if pivot != i:
+            A[pivot], A[i] = A[i], A[pivot]
+
+        # Eliminating
+        for j in range(size):
+            if j == i:
+                continue
+            factor = A[j][i] / A[i][i]
+            A[j] = [a - b for a, b in zip(A[j], [k * factor for k in A[i]])]
+
+        # Scaling
+        factor = 1 / A[i][i]
+        A[i] = [k * factor for k in A[i]]
+
+    X = [A[i][-1] for i in range(size)]
+    return X
--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 from PyQt5 import QtWidgets
 import gui2
-
+import functions
 
 if __name__ == "__main__":
     import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = gui2.Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+    # app = QtWidgets.QApplication(sys.argv)
+    # MainWindow = QtWidgets.QMainWindow()
+    # ui = gui2.Ui_MainWindow()
+    # ui.setupUi(MainWindow)
+    # MainWindow.show()
+    # sys.exit(app.exec_())
 
+    A = [
+        [1, 1, 2],
+        [-1, -2, 3],
+        [3, 7, 4]
+    ]
+    AA = [
+        8, 1, 10
+    ]
+    B = [
+        [1, 1, 2, 8],
+        [-1, -2, 3, 1],
+        [3, 7, 4, 10]
+    ]
+    C = [
+        [1, 1, 2, 8],
+        [-1, -2, 3, 1],
+        [3, 7, 4, 10]
+    ]
+
+    print(functions.LU_Decomposition(A, AA))
+    print(functions.Gauss_Elimination(B))
+    print(functions.Gauss_Jordan(C))
 
